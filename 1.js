1d4f59139650509fbfcd5e34cc7fa41a1e20aa8a
dabuzida <dabuzida@gmail.com>
2022-06-13 18:02:32 +0900

    비동기 플밍

--- 1.js
+++ 1.js
@@ -7,10 +7,16 @@ async function run() {
     setTimeout(() => resolve("p2 resolved ok!"), 1000);
   });
   console.log(p2);
+  let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p3 resolved ok!"), 1000);
+  });
+  console.log(p3);
 
   let r1 = await p1;
   console.log(r1);
   let r2 = await p2;
   console.log(r2);
+  let r3 = await p3;
+  console.log(r3);
 }
 run();
